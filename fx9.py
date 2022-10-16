c916ca59ef420f762253ea0c6184a8dc1a74ee11
Fanan-soufiane <115195347+Fanan-soufiane@users.noreply.github.com>
2022-10-16 01:06:30 +0100

    Add files via upload

---
+++ fx9.py
@@ -0,0 +1,30 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def homescreen () :
+    os.system ('clear')
+    print ("          .-;;-. ")
+    print ("         / .--. \  ")
+    print ("        / /    \ \  ")
+    print ("        | |    | |  ")
+    print ("        | |.-""-.|  ")
+    print ("       ///`.::::.`\   ") 
+    print ("      ||| ::/  \ ::;  ")
+    print ("      ||; ::\__/ ::;  ")
+    print ("        \ \.:::::: /  ")
+    print ("        `=`:-..-``  ")
+    print ('  script  فدﺤ مﺗ')
+    print (' ')
+    V = input ("\033[1;36m  errors \033[1;32m ==> ")
+    if V == 'my ' :
+        os.system ('errors')
+    else:
+        print ('errors ')
+        timeout(0.6)
+        homescreen ()
+
+homescreen ()
